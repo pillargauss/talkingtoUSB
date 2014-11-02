20310e24a21779a9227e2b915cf5d5e53c0835dc
pillargauss <gao-j@email.ulster.ac.uk>
2014-11-03 07:51:05 +0800

    Add new module

--- TalkToUSB/Form1.Designer.vb
+++ TalkToUSB/Form1.Designer.vb
@@ -22,9 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.usb = New System.IO.Ports.SerialPort(Me.components)
+        Me.bluetooth = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -41,7 +51,7 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(69, 22)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "send"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox2
@@ -51,11 +61,89 @@ Partial Class Form1
         Me.TextBox2.Size = New System.Drawing.Size(122, 21)
         Me.TextBox2.TabIndex = 2
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(50, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(80, 24)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "connect USB"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'usb
+        '
+        Me.usb.PortName = "COM6"
+        '
+        'bluetooth
+        '
+        Me.bluetooth.BaudRate = 230400
+        Me.bluetooth.PortName = "COM5"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(296, 61)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Connect BT"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(268, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "BT received"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(270, 127)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(243, 189)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 12)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(57, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(57, 102)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 12)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Label4"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(593, 383)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
@@ -68,5 +156,14 @@ Partial Class Form1
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents usb As System.IO.Ports.SerialPort
+    Friend WithEvents bluetooth As System.IO.Ports.SerialPort
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
 
 End Class
--- TalkToUSB/Form1.vb
+++ TalkToUSB/Form1.vb
@@ -1,3 +1,222 @@
-﻿Public Class Form1
+﻿Imports HIDLibrary
+Imports System.Management
+Imports System.Threading
 
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+    Private Declare Function FT_Open Lib "FTD2XX.dll" (ByVal intDeviceNumber As Short, ByRef lnghandle As Integer) As Integer
+    Const FT_Ok As Short = 0
+    Dim buf(100) As Byte
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim sendBytes, readBytes As Byte()
+        Dim BytesWritten As Integer
+
+        sendBytes = System.Text.Encoding.Default.GetBytes(TextBox2.Text)
+ 
+        ' Purge buffers
+        FT_Status = FT_Purge(FT_Handle, FT_PURGE_RX Or FT_PURGE_TX)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Set Baud Rate
+        FT_Status = FT_SetBaudRate(FT_Handle, 115200)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Set parameters
+        FT_Status = FT_SetDataCharacteristics(FT_Handle, FT_DATA_BITS_8, FT_STOP_BITS_1, FT_PARITY_NONE)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Set Flow Control
+        FT_Status = FT_SetFlowControl(FT_Handle, FT_FLOW_RTS_CTS, 0, 0)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Set RTS
+        FT_Status = FT_SetRts(FT_Handle)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Set DTR
+        FT_Status = FT_SetDtr(FT_Handle)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Write string data to device
+        FT_Status = FT_Write_String(FT_Handle, TextBox2.Text, Len(TextBox2.Text), BytesWritten)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Wait
+        Sleep(100)
+
+        ' Get number of bytes waiting to be read
+        FT_Status = FT_GetQueueStatus(FT_Handle, FT_RxQ_Bytes)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+
+        ' Read number of bytes waiting
+        ' Allocate string to recieve data
+        Dim TempStringData As String
+        Dim BytesRead As Integer
+
+        TempStringData = Space(FT_RxQ_Bytes + 1)
+        FT_Status = FT_Read_String(FT_Handle, TempStringData, FT_RxQ_Bytes, BytesRead)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+        ' Display string on form
+        TextBox1.Text = Trim(TempStringData)
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        'myUSB.Open("Vid_0403&Pid_6001")
+        'Dim USBClass As New System.Management.ManagementClass("Win32_USBHub")
+        'Dim USBCollection As System.Management.ManagementObjectCollection = USBClass.GetInstances()
+        'Dim USB As System.Management.ManagementObject
+        'Dim temp As String = "USB\VID_0403&PID_6001\A101EOSM"
+
+        'For Each USB In USBCollection
+        '    Me.ListBox2.Items.Add("Description = " & USB("Name").ToString())
+        '    Me.ListBox2.Items.Add("Device ID = " & USB("deviceid").ToString())
+        '    Me.ListBox2.Items.Add("PNP Device ID = " & USB("PNPDeviceID").ToString())
+        '    'temp = USB.Properties("VID").Value.ToString()
+        '    If StrComp(temp, USB("deviceid").ToString()) = 0 Then
+        '        TextBox1.AppendText(USB("deviceid").ToString() & Environment.NewLine)
+        '    End If
+
+        'Next USB
+
+        'Dim microChipUSB As HIDLibrary.HidDevice()
+        'Dim myUSB As HidDevice
+        'microChipUSB = HidDevices.Enumerate(USB("deviceid"))
+        'myUSB = microChipUSB(0)
+        Dim lnghandle As Object
+        Dim ftdiDeviceCount As Int16
+        Dim TempManufacturer As String
+        Dim TempManufacturerID As String
+        Dim TempDescription As String
+        Dim TempSerialNumber As String
+
+        Dim DeviceIndex As Integer
+        Dim TempDevString As String
+        Dim lngFT_Type, ID As Integer
+        Dim VidPid As String
+
+
+        ' Create empty strings
+        TempManufacturer = Space(32)
+        TempManufacturerID = Space(16)
+        TempDescription = Space(64)
+        TempSerialNumber = Space(16)
+
+        'Get the number of device connected. 
+        'FT_ListDevices - Get information concerning the devices currently connected.  This function can return such information as the 
+        'number of devices connected, and device strings such as serial number and product description.
+        FT_Status = FT_GetNumberOfDevices(ftdiDeviceCount, vbNullChar, FT_LIST_NUMBER_ONLY)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+        'Display the device number found
+        Label2.Text = ftdiDeviceCount.ToString + "FTDI device found"
+
+        TempDevString = Space(16)
+        FT_Status = FT_GetDeviceString(DeviceIndex, TempDevString, FT_LIST_BY_INDEX Or FT_OPEN_BY_SERIAL_NUMBER)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+        FT_Status = FT_OpenByIndex(DeviceIndex, FT_Handle)
+
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+        FT_Status = FT_GetDeviceInfo(FT_Handle, lngFT_Type, ID, TempSerialNumber, TempDescription, 0)
+        If FT_Status <> FT_Ok Then
+            Exit Sub
+        End If
+
+        VidPid = Hex(ID)
+        If Len(VidPid) < 8 Then
+            VidPid = "0" + VidPid
+        End If
+
+        Label3.Text = "Vendor ID:" + Microsoft.VisualBasic.Left(VidPid, 4)
+        Label4.Text = "Product ID:" + Microsoft.VisualBasic.Right(VidPid, 4)
+
+       
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If Not bluetooth.IsOpen Then
+            bluetooth.Open()
+            AddHandler bluetooth.DataReceived, AddressOf bluetooth_DataReceived
+        End If
+    End Sub
+
+    Private Sub bluetooth_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles bluetooth.DataReceived
+        If bluetooth.IsOpen Then
+            Try
+                ThreadProcSafe(0)
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+    Private Sub ThreadProcSafe(ByVal x As String)
+        Me.SetText("Text set safely " & x)
+    End Sub
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+    Private Sub SetText(ByVal [text] As String)
+        'TextBox3.AppendText(bluetooth.ReadLine())
+
+        Dim bufSize As Integer = bluetooth.BytesToRead
+        If bufSize > 0 Then
+            bluetooth.Read(buf, 0, bufSize)
+        End If
+        TextBox3.AppendText(buf.ToString)
+    End Sub
+
+    'Private Sub usb_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles usb.DataReceived
+    '    Try
+    '        ThreadProcSafe_usb(0)
+    '    Catch ex As Exception
+
+    '    End Try
+    'End Sub
+    'Private Sub ThreadProcSafe_usb(ByVal x As String)
+    '    Me.settext1("Text set safely" & x)
+    'End Sub
+    'Private Sub settext1(ByVal [text] As String)
+    '    TextBox3.AppendText(usb.ReadLine() & "from usb" & Environment.NewLine)
+    'End Sub
 End Class
